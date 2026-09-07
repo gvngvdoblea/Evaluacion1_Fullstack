79e67a7ed0787f580d03dc25f18e256bd60a3e63
Alejandro Andres <alejandroandres026@gmail.com>
2026-09-07 15:08:17 -0300

    fix: soluciono error js y agrego pagina crearUsuario

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const btnMenu = document.getElementById("btnMenu");
 const navLinks = document.getElementById("navLinks");
 
-btnMenu.addEventListener("click", function() {
-  navLinks.classList.toggle("activo");
-});
-```[cite: 2]
+if (btnMenu && navLinks) {
+  btnMenu.addEventListener("click", function() {
+    navLinks.classList.toggle("activo");
+  });
+}
